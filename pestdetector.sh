2ddef60034f129cfcbccf0f98ae4d598147cd070
Christian Baun <christianbaun@gmail.com>
2021-12-05 17:50:13 +0100

    Add TFLite_detection_image_modified and LICENSE file for Apache 2 license and improved pestdetector.

--- pestdetector.sh
+++ pestdetector.sh
@@ -15,12 +15,25 @@
 # example:      ./pestdetector.sh
 # ----------------------------------------------------------------------------
 
-# Path of the directory for the images
+# Path of the directory for the most recent picture
+DIRECTORY_MOST_RECENT_IMAGE="most_recent_image"
+# Path of the directory for the picture
 DIRECTORY_IMAGES="images"
-#IRECTORY_IMAGES_MAX_SIZE="1073741824"  # 1 GB max
+#DIRECTORY_IMAGES_MAX_SIZE="1073741824"  # 1 GB max
 DIRECTORY_IMAGES_MAX_SIZE="5000"  # 5 MB max for testing purposes
 DIRECTORY_LOGS="logs"
 DIRECTORY_LOGS_MAX_SIZE="1048576" # 1 MB max
+
+STANDARDMODELL=model_2021_07_08_rat_bug_hedgehog
+
+if [ -z "$1" ]; then
+  # No model provided as parameter => mode default model: $STANDARDMODELL
+  MODELLNAME=$STANDARDMODELL
+fi
+
+MODEL="/home/pi/$MODELLNAME"
+LABELS="/home/pi/$MODELLNAME/labelmap.txt"
+
 
 RED='\033[0;31m'          # Red color
 NC='\033[0m'              # No color
@@ -37,6 +50,31 @@ else
     HOSTNAME=$(hostname)
     echo -e "Welcome to pestdetector on host ${HOSTNAME}"
 fi
+
+# ------------------------------
+# | Check the operating system |
+# ------------------------------
+
+if [[ "$OSTYPE" == "linux-gnu" || "$OSTYPE" == "linux-gnueabihf" ]]; then
+    # Linux
+    echo -e "${YELLOW}[INFO] The operating system is Linux: ${OSTYPE}${NC}"
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
+    # FreeBSD
+    echo -e "${YELLOW}[INFO] The operating system is FreeBSD: ${OSTYPE}${NC}"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    # Mac OS X
+    echo -e "${YELLOW}[INFO] The operating system is Mac OS X: ${OSTYPE}${NC}"
+elif [[ "$OSTYPE" == "msys" ]]; then
+    # Windows 
+    echo -e "${YELLOW}[INFO] The operating system is Windows: ${OSTYPE}${NC}"
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+    # POSIX compatibility layer for Windows
+    echo -e "${YELLOW}[INFO] POSIX compatibility layer for Windows detected: ${OSTYPE}${NC}"
+else
+    # Unknown
+    echo -e "${YELLOW}[INFO] The operating system is unknown: ${OSTYPE}${NC}"
+fi
+
 
 # ----------------------------------------------------
 # | Check that we have a working internet connection |
@@ -92,6 +130,20 @@ else
   fi
 fi
 
+
+# Check if the most_recent_image directory already exists
+if [ -e ${DIRECTORY_MOST_RECENT_IMAGE} ] ; then
+  # If the directory for the most_recent_image already exists
+   echo -e "${YELLOW}[INFO] The directory ${DIRECTORY_MOST_RECENT_IMAGE} already exists in the local directory.${NC}"
+else
+  # If the directory for the most_recent_image does not already exist => create it
+  if mkdir ${DIRECTORY_MOST_RECENT_IMAGE} ; then
+    echo -e "${GREEN}[OK] The local directory ${DIRECTORY_MOST_RECENT_IMAGE} has been created.${NC}"
+  else
+    echo -e "${RED}[ERROR] Unable to create the local directory ${DIRECTORY_MOST_RECENT_IMAGE}.${NC}" && exit 1
+  fi
+fi
+
 # Check if the logs directory already exists
 if [ -e ${DIRECTORY_LOGS} ] ; then
   # If the directory for the logs already exists
@@ -111,36 +163,67 @@ fi
 
 # Store timestamp of the date and time in a variable
 DATE_AND_TIME_STAMP=$(date +%Y-%m-%d_%H-%M-%S)
+IMAGE_FILENAME_AND_PATH="${DIRECTORY_MOST_RECENT_IMAGE}/${DATE_AND_TIME_STAMP}.jpg"
 
-if raspistill -o ${DIRECTORY_IMAGES}/${DATE_AND_TIME_STAMP}.jpg -n ; then
-  echo -e "${GREEN}[OK] The picture ${DIRECTORY_IMAGES}/${DATE_AND_TIME_STAMP}.jpg has been created.${NC}"
+if raspistill -o ${IMAGE_FILENAME_AND_PATH} -n ; then
+  echo -e "${GREEN}[OK] The picture ${IMAGE_FILENAME_AND_PATH} has been created.${NC}"
 else
-  echo -e "${RED}[ERROR] Unable to create the picture ${DIRECTORY_IMAGES}/${DATE_AND_TIME_STAMP}.jpg.${NC}" && exit 1
+  echo -e "${RED}[ERROR] Unable to create the picture ${IMAGE_FILENAME_AND_PATH}.${NC}" && exit 1
 fi
+
+# -----------------------------------------
+# | Object detection and logfile creation |
+# -----------------------------------------
+
+# Filename of the log file
+LOG_FILENAME_AND_PATH="${DIRECTORY_MOST_RECENT_IMAGE}/${DATE_AND_TIME_STAMP}.txt"
+
+if [[ -f "${IMAGE_FILENAME_AND_PATH}" ]] ; then
+    python3 TFLite_detection_image_modified.py --modeldir=$MODEL --graph=detect_edgetpu.tflite --labels=$LABELS --edgetpu --image=${IMAGE_FILENAME_AND_PATH} 2>&1 | tee -a $LOG_FILENAME_AND_PATH
+else
+    echo "The image file ${IMAGE_FILENAME_AND_PATH} was not found."
+    exit 
+fi
+
+# -----------------------------------------
+# | Prevent the directories from overflow |
+# -----------------------------------------
 
 # Get the sum of the bytes in the images directory and keep only the first column of the output with awk
 DIRECTORY_IMAGES_ACTUAL_SIZE=$(du -s ${DIRECTORY_IMAGES} | awk '{ print $1 }')
 
 # Get the number of files in the images directory
-# -U causes ls to not sort the entries => less memory consumption
-# -b prints C-style escapes for nongraphic characters, => newlines are printed as \n
-# -a prints out all files, even hidden files 
-# -d prints out directories without attempting to list the contents of the directory
-# -1 makes sure that it's on one column 
-# 
-DIRECTORY_IMAGES_NUMBER_OF_FILES=$(ls -Ubad1 images/* | wc -l)
+# But first, check if the images directory is not empty and contains a least a single jpg file
+if [[ -f images/*.jpg ]] ; then
+  # -U causes ls to not sort the entries => less memory consumption
+  # -b prints C-style escapes for nongraphic characters, => newlines are printed as \n
+  # -a prints out all files, even hidden files 
+  # -d prints out directories without attempting to list the contents of the directory
+  # -1 makes sure that it's on one column 
+  DIRECTORY_IMAGES_NUMBER_OF_FILES=$(ls -Ubad1 ${DIRECTORY_IMAGES}/*.jpg | wc -l)
+ 
+  # Get the sum of the bytes in the images directory and keep only the first column of the output with awk
+  DIRECTORY_IMAGES_ACTUAL_SIZE=$(du -s ${DIRECTORY_IMAGES} | awk '{ print $1 }')
 
-echo -e "${YELLOW}[INFO] Files in ${DIRECTORY_IMAGES}: ${DIRECTORY_IMAGES_NUMBER_OF_FILES}${NC}"
-echo -e "${YELLOW}[INFO] Used Bytes in ${DIRECTORY_IMAGES}: ${DIRECTORY_IMAGES_ACTUAL_SIZE}${NC}"   
+  echo -e "${YELLOW}[INFO] Files in ${DIRECTORY_IMAGES}: ${DIRECTORY_IMAGES_NUMBER_OF_FILES}${NC}"
+  echo -e "${YELLOW}[INFO] Used Bytes in ${DIRECTORY_IMAGES}: ${DIRECTORY_IMAGES_ACTUAL_SIZE}${NC}"   
+
+else
+  echo -e "${YELLOW}[INFO] The directory ${DIRECTORY_IMAGES} is still empty${NC}"
+fi
+  
 
 if [[ "${DIRECTORY_IMAGES_ACTUAL_SIZE}" -lt "${DIRECTORY_IMAGES_MAX_SIZE}" ]] ; then
   echo -e "${GREEN}[OK] There is enough free storage capacity in the directory ${DIRECTORY_IMAGES}${NC}"
 else
+  echo -e "${YELLOW}[INFO] Attention: The directory ${DIRECTORY_IMAGES} consumes ${DIRECTORY_IMAGES_ACTUAL_SIZE} Bytes which is more than the permitted maximum ${DIRECTORY_IMAGES_MAX_SIZE} Bytes !${NC}"  
   while [ "${DIRECTORY_IMAGES_ACTUAL_SIZE}" -gt "${DIRECTORY_IMAGES_MAX_SIZE}" ]; do 
-    echo -e "${RED}[INFO] Attention: The directory ${DIRECTORY_IMAGES} consumes ${DIRECTORY_IMAGES_ACTUAL_SIZE} Bytes which is more than the permitted maximum ${DIRECTORY_IMAGES_MAX_SIZE} Bytes !${NC}"  
     DIRECTORY_IMAGES_OLDEST_FILE=$(ls -t ${DIRECTORY_IMAGES} | tail -1)
     if rm ${DIRECTORY_IMAGES}/${DIRECTORY_IMAGES_OLDEST_FILE}; then
-      echo -e "${GREEN}[INFO] Erased the file ${DIRECTORY_IMAGES_OLDEST_FILE} from ${DIRECTORY_IMAGES}${NC}"
+      echo -e "${YELLOW}[INFO] Erased the file ${DIRECTORY_IMAGES_OLDEST_FILE} from ${DIRECTORY_IMAGES}${NC}"
+      
+      # TODO: Erase the logfile for this image too
+
       # Fetch the new sum of the bytes in the images directory and keep only the first column of the output with awk
       DIRECTORY_IMAGES_ACTUAL_SIZE=$(du -s ${DIRECTORY_IMAGES} | awk '{ print $1 }')
     else 
@@ -148,6 +231,7 @@ else
       exit 1
     fi
   done
+  echo -e "${YELLOW}[INFO] Now, the directory ${DIRECTORY_IMAGES} consumes ${DIRECTORY_IMAGES_ACTUAL_SIZE} Bytes which is less than the permitted maximum ${DIRECTORY_IMAGES_MAX_SIZE} Bytes !${NC}" 
 fi
 
 
@@ -156,8 +240,6 @@ fi
 # Store timestamp of the date in a variable
 DATE_TIME_STAMP=$(date +%Y-%m-%d)
 
-# Filename of the log file
-LOG_FILENAME="${DATE_TIME_STAMP}_results.txt"
 
 
 
