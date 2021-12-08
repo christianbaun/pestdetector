de31c277715c11a9db988b0d2f1f696d1f2aceb8
Christian Baun <christianbaun@gmail.com>
2021-12-08 10:37:21 +0100

    A second I2C LCD display works now well and informs about the program state.

---
+++ lcd_output_display1.py
@@ -0,0 +1,17 @@
+import sys
+sys.path.append("./lib")
+import lcddriver1
+from time import *
+
+lcd = lcddriver1.lcd()
+lcd.lcd_clear()
+
+line1=sys.argv[1]
+line2=sys.argv[2]
+line3=sys.argv[3]
+line4=sys.argv[4]
+ 
+lcd.lcd_display_string(line1, 1)
+lcd.lcd_display_string(line2, 2)
+lcd.lcd_display_string(line3, 3)
+lcd.lcd_display_string(line4, 4)
